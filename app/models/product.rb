cb44904d6657a3d0c52cd8ddb51baed6ed6c8b6f
unknown <Timothy@.(none)>
2009-04-20 21:35:14 -0400

    Initial Import

---
+++ app/models/product.rb
@@ -0,0 +1,22 @@
+class Product < ActiveRecord::Base
+
+  def self.find_products_for_sale
+    find(:all, :order => "title")
+  end
+  
+  validates_presence_of :title, :description,:image_url
+  validates_numericality_of :price
+  validate :price_must_be_at_least_a_cent
+  validates_uniqueness_of :title
+  validates_format_of :image_url, :with => %r{\.(gif|jpg|png)$}i,:message => 'must be a url for gif, jpg or png.'
+  
+  def self.find_products_for_sale
+    find(:all, :order => "title")
+  end
+  
+protected
+  def price_must_be_at_least_a_cent
+    errors.add(:price, 'should be at least $0.01') if price.nil? || price < 0.01
+  end
+ 
+end
