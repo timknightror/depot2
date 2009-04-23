45170707f5c66e8a7cae15edbe7b754ba0e79423
Timothy Knight <github@52brookedge.com>
2009-04-22 21:24:35 -0400

    Desktop Initial push

---
+++ app/controllers/store_controller.rb
@@ -0,0 +1,45 @@
+class StoreController < ApplicationController
+  
+  def index
+    @products = Product.find_products_for_sale
+    @cart = find_cart
+  end
+  
+  def add_to_cart
+    product = Product.find(params[:id])
+    @cart = find_cart
+    @current_item = @cart.add_product(product)
+      respond_to do |format|
+        format.js if request.xhr?
+        format.html {redirect_to_index}
+      end
+  rescue ActiveRecord::RecordNotFound
+    logger.error("Attempt to access invalid product #{params[:id]}")
+    redirect_to_index("Invalid product")
+  end
+  
+
+
+  
+  def empty_cart
+    session[:cart] = nil
+    redirect_to_index
+  end
+  
+
+private
+
+  
+  def redirect_to_index(msg = nil)
+    flash[:notice] = msg if msg
+    redirect_to :action => 'index'
+  end
+  
+  
+
+  def find_cart
+    session[:cart] ||= Cart.new
+  end
+
+
+end
